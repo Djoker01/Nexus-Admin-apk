c951b0c49e45095bad2174bb356f037e951c2f38
Djoker01 <113226157+Djoker01@users.noreply.github.com>
2026-07-30 15:28:59 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,12 @@ dependencies {
     
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    
+    // Apache POI para Excel
+    implementation("org.apache.poi:poi:5.2.5")
+    implementation("org.apache.poi:poi-ooxml:5.2.5")
+    
+    // iText para PDF
+    implementation("com.itextpdf:itext7-core:7.2.5")
 }
+
